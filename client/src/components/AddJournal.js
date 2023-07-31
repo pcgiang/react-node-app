7c8eb3505bf939b88c80369f37e6e023e9d929b2
pcgiang <chaugiang0512@gmail.com>
2023-07-31 23:10:54 +0800

    added mood selector

--- client/src/components/AddJournal.js
+++ client/src/components/AddJournal.js
@@ -1,63 +1,91 @@
 import React from 'react'
 import { useState } from 'react'
 import journalStyle from '../styles/journalStyle.css'
+import MoodSelecter from './MoodSelecter'
 
 const AddJournal = ({ addJournal }) => {
   const [ content, setContent ] = useState("")
+  const [ error, setError ] = useState(null)
+  const [ isPopup, setIsPopup ] = useState(false)
   const [ isSubmit, setIsSubmit ] = useState(false)
 
-  const [ isPopup, setIsPopup ] = useState(false)
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault(); // prevent page reload when form is submited
-    addJournal(new Date().toDateString(), content)
-    setIsSubmit(true)
+    const journal = { content, happiness } 
 
-    setContent('')
+    const response = await fetch('/api/journals', {
+      method: 'POST',
+      body: JSON.stringify(journal),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    const json = await response.json()
+    if (!response.ok) {
+      setError(json.error)
+      console.log(error)
+    }
+    if (response.ok) {
+      setContent('')
+      setHappiness(null)
+      setError(null)
+      setIsSubmit(true)
+      console.log(' new journal added ')
+      console.log(journal)
+    }
   }
 
-    return (
-      <div  className="add-journal">
-      {
-        !isPopup && (
-          <div className='journal-input-container'>
-            <input
-              type="text" className='journal-input'
-              placeholder='How was your day today'
-              onClick={() => setIsPopup(true)}
-            />
-          </div>
+  const [ happiness, setHappiness ] = useState(null)
+  const handleHappinessSelect = (e) => {
+    e.preventDefault()
+    setHappiness(e.target.innerText)
+    console.log(happiness)
+  }
 
-        )
-      }
+  return (
+    <div  className="add-journal">
+    {
+      !isPopup && (
+        <div className='journal-input-container'>
+          <input
+            type="text" className='journal-input'
+            placeholder='How was your day today'
+            onClick={() => setIsPopup(true)}
+          />
+        </div>
 
-      {
-        isPopup && (
-          <div className='journal-form-pop-up'>
-            <div className='journal-form-pop-up-card'>
-              <div className='journal-pop-up-header'>
-                <h3> Your journal </h3>
-                <button onClick={() => setIsPopup(false)} className='journal-popup-close-btn'> x </button>
-              </div>
-              <form className='journal-form' onSubmit={handleSubmit}>
-                <textarea 
-                  type='text' className='journal-input-popup' 
-                  placeholder='How was your day today?' 
-                  onChange={(e) => setContent(e.target.value)}
-                  value={content}
-                />
-                <br/>
-                <button type='submit' className='primary-button'> Submit Journal </button>
-              </form>
+      )
+    }
+
+    {
+      isPopup && (
+        <div className='journal-form-pop-up'>
+          <div className='journal-form-pop-up-card'>
+            <div className='journal-pop-up-header'>
+              <h3> Your journal </h3>
+              <button onClick={() => setIsPopup(false)} className='journal-popup-close-btn'> x </button>
             </div>
+            <form className='journal-form'>
+              <textarea 
+                type='text' className='journal-input-popup' 
+                placeholder='How was your day today?' 
+                onChange={(e) => setContent(e.target.value)}
+                value={content}
+              />
+              <br/>
 
+              <MoodSelecter handleHappinessSelect={handleHappinessSelect} isSubmit={isSubmit}/>
+              <button type='submit' className='primary-button' onClick={handleSubmit}> Submit Journal </button>
+            </form>
           </div>
-        )
-      }
 
-      </div>
+        </div>
+      )
+    }
 
-    )
+    </div>
+
+  )
 }
 
 export default AddJournal
---
+++ client/src/components/MoodSelecter.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+
+const MoodSelecter = ({ handleHappinessSelect, isSubmit}) => {
+  const moodRankings = [1, 2, 3, 4, 5]
+
+  const [ isActive, setIsActive ] = useState(null)
+
+  useEffect(() => {
+    if (isSubmit) {
+      setIsActive(null)
+    }
+  }, [isSubmit])
+
+  return (
+    <div>
+      <div> Rank your mood below : </div>
+      <div className='mood-container'>
+        {
+          moodRankings.map((rank) => (
+            <button
+              key={rank}
+              className={rank === isActive ? 'mood-ranking-btn btn-active' : 'mood-ranking-btn'}
+              onClick={(event) => { handleHappinessSelect(event); setIsActive(rank) } }
+            > { rank } </button>
+          ))
+        }
+      </div>
+    </div>
+
+  )
+}
+
+export default MoodSelecter
